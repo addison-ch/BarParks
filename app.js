1de069ae7ff9db24d03a3a3658406d0197a3b140
addison-ch <addisonchen.py@gmail.com>
2021-07-04 18:42:07 -0400

    implementing crud functionality

--- app.js
+++ app.js
@@ -18,15 +18,35 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     res.render('home.ejs');
 })
-app.get('/makepark', async (req, res) => {
-    const park = new Park({ title: 'ACCI', description: 'good park' });
-    await park.save();
-    res.send(park)
+
+app.get('/parks', async (req, res) => {
+    const parks = await Park.find({});
+    res.render('parks/index.ejs', { parks });
 })
+
+app.post('/parks', async (req, res) => {
+    const park = new Park(req.body.park);
+    await park.save();
+    res.redirect(`/parks/${park._id}`);
+
+})
+app.get('/parks/new', (req, res) => {
+    res.render('parks/new.ejs');
+})
+
+app.get('/parks/:id', async (req, res) => {
+    const { id } = req.params;
+    const park = await Park.findById(id);
+    res.render('parks/show.ejs', { park })
+})
+
+
 app.listen(3000, function () {
     console.log('Serving on port 3000');
 })
