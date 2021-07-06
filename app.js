f4069e4ca76208e0ae6041ae3d733fdf97e20539
addison-ch <addisonchen.py@gmail.com>
2021-07-05 22:53:15 -0400

    crud infrastructure completed

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+mongoose.set('useFindAndModify', false);
 
 app.get('/', (req, res) => {
     res.render('home.ejs');
