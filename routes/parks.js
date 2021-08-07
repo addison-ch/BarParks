d57762fab19b954ba93ad739b7d7c70750b2c162
Addison <74084822+addison-ch@users.noreply.github.com>
2021-08-07 19:10:08 -0400

    restructured routes

--- routes/parks.js
+++ routes/parks.js
@@ -1,28 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const Park = require('../models/park');
-const ejsMate = require('ejs-mate');
-const { parkSchema, reviewSchema } = require('../schemas.js')
-const methodOverride = require('method-override');
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
 const { isLoggedIn, isAuthor, validatePark } = require('../middleware.js');
 const parks = require('../controllers/parks.js');
 
 
-
-router.get('/', catchAsync(parks.index))
-
-router.post('/', isLoggedIn, validatePark, catchAsync(parks.submitPark))
+router.route('/')
+    .get(catchAsync(parks.index))
+    .post(isLoggedIn, validatePark, catchAsync(parks.submitPark))
 
 router.get('/new', isLoggedIn, parks.newParkForm)
 
+router.route('/:id')
+    .get(catchAsync(parks.showPark))
+    .put(isLoggedIn, validatePark, isAuthor, catchAsync(parks.updatePark))
+    .delete(isLoggedIn, isAuthor, catchAsync(parks.deletePark))
+
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(parks.editPark))
-
-router.get('/:id', catchAsync(parks.showPark))
-
-router.put('/:id', isLoggedIn, validatePark, isAuthor, catchAsync(parks.updatePark))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(parks.deletePark))
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const auth = require('../controllers/auth.js');
+const router = express.Router()
 
-router.get('/register', auth.loadRegister)
+router.route('/register')
+    .get(auth.loadRegister)
+    .post(catchAsync(auth.register))
 
-router.post('/register', catchAsync(auth.register))
-
-router.get('/login', auth.loadLogin)
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), auth.login)
+router.route('/login')
+    .get(auth.loadLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), auth.login)
 
 router.get('/logout', auth.logout)
 
