eb313edc3c2437251b1d3482c047e418141dbdaa
Addison <74084822+addison-ch@users.noreply.github.com>
2021-10-12 02:19:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const store = MongoStore.create({
 const sessionConfig = {
     store: store,
     name: 'sess',
-    secret,
+    secretkey,
     resave: false, saveUninitialized: true,
     cookie: {
         httpOnly: true,
